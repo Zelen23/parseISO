8be7ebc083c5e2b8dd342792912509882061d6a2
Alexander Zelinskiy <a.zelinskiy@it.ecommpay.com>
2019-12-05 17:54:15 +0300

    addd

--- src/main/java/com/mycompany/iso8583/ISOIss.java
+++ src/main/java/com/mycompany/iso8583/ISOIss.java
@@ -5,16 +5,33 @@
  */
 package com.mycompany.iso8583;
 
+import org.jpos.iso.IFA_BITMAP;
 import org.jpos.iso.IFA_FLLCHAR;
+import org.jpos.iso.IFA_LLCHAR;
+import org.jpos.iso.IFA_LLLCHAR;
+import org.jpos.iso.IFA_LLLLCHAR;
+import org.jpos.iso.IFA_LLNUM;
+import org.jpos.iso.IFA_NUMERIC;
 import org.jpos.iso.IFB_AMOUNT;
 import org.jpos.iso.IFB_BINARY;
 import org.jpos.iso.IFB_BITMAP;
+import org.jpos.iso.IFB_HEX;
 import org.jpos.iso.IFB_LLCHAR;
+import org.jpos.iso.IFB_LLHBINARY;
+import org.jpos.iso.IFB_LLHEX;
 import org.jpos.iso.IFB_LLLCHAR;
+import org.jpos.iso.IFB_LLLLCHAR;
+import org.jpos.iso.IFB_LLLNUM;
 import org.jpos.iso.IFB_LLNUM;
 import org.jpos.iso.IFB_NUMERIC;
+import org.jpos.iso.IFE_BITMAP;
 import org.jpos.iso.IFE_CHAR;
 import org.jpos.iso.IFE_CHAR;
+import org.jpos.iso.IFE_LLLBINARY;
+import org.jpos.iso.IFE_LLLCHAR;
+import org.jpos.iso.IFE_LLLLCHAR;
+import org.jpos.iso.IFE_LLLNUM;
+import org.jpos.iso.IFE_NUMERIC;
 import org.jpos.iso.IF_CHAR;
 import org.jpos.iso.IF_ECHAR;
 import org.jpos.iso.ISOBasePackager;
@@ -27,17 +44,17 @@ import org.jpos.iso.ISOFieldPackager;
 public class ISOIss  extends ISOBasePackager{
         private static final boolean pad = false;
     protected ISOFieldPackager fld[] = {
-            new IFB_NUMERIC (  4, "MESSAGE TYPE INDICATOR", true),
+            new IFB_NUMERIC (  4, "MESSAGE TYPE INDICATOR",true),
             new IFB_BITMAP  ( 16, "BIT MAP"),
-            new IFB_NUMERIC   ( 19, "PAN - PRIMARY ACCOUNT NUMBER", true),
+            new IFB_LLNUM   ( 19, "PAN - PRIMARY ACCOUNT NUMBER",true),
             new IFB_NUMERIC (  6, "PROCESSING CODE", true),
             new IFB_NUMERIC ( 12, "AMOUNT, TRANSACTION", true),
             new IFB_NUMERIC ( 12, "AMOUNT, SETTLEMENT", true),
-            new IFB_NUMERIC ( 12, "AMOUNT, CARDHOLDER BILLING", true),
-            new IFB_NUMERIC ( 10, "TRANSMISSION DATE AND TIME", true),
+            new IFB_NUMERIC ( 12, "AMOUNT, CARDHOLDER BILLING", true),            
+ /*de007*/  new IFB_NUMERIC ( 10, "TRANSMISSION DATE AND TIME", true),           
             new IFB_NUMERIC (  8, "AMOUNT, CARDHOLDER BILLING FEE", true),
             new IFB_NUMERIC (  8, "CONVERSION RATE, SETTLEMENT", true),
-            new IFB_NUMERIC (  8, "CONVERSION RATE, CARDHOLDER BILLING", true),
+/*de010*/   new IFB_NUMERIC (  8, "CONVERSION RATE, CARDHOLDER BILLING", true),
             new IFB_NUMERIC (  6, "SYSTEM TRACE AUDIT NUMBER", true),
             new IFB_NUMERIC (  6, "TIME, LOCAL TRANSACTION", true),
             new IFB_NUMERIC (  4, "DATE, LOCAL TRANSACTION", true),
@@ -45,25 +62,25 @@ public class ISOIss  extends ISOBasePackager{
             new IFB_NUMERIC (  4, "DATE, SETTLEMENT", true),
             new IFB_NUMERIC (  4, "DATE, CONVERSION", true),
             new IFB_NUMERIC (  4, "DATE, CAPTURE", true),
-            new IFB_NUMERIC (  4, "MERCHANTS TYPE", true),
+/*de018*/   new IFB_NUMERIC (  4, "MERCHANTS TYPE", true),
             new IFB_NUMERIC (  3, "ACQUIRING INSTITUTION COUNTRY CODE", true),
             new IFB_NUMERIC (  3, "PAN EXTENDED COUNTRY CODE", true),
             new IFB_NUMERIC (  3, "FORWARDING INSTITUTION COUNTRY CODE", true),
             new IFB_NUMERIC (  4, "POINT OF SERVICE ENTRY MODE", true),
-            new IFB_NUMERIC (  3, "CARD SEQUENCE NUMBER", true),
+   /*de023*/new IFB_NUMERIC (  3, "CARD SEQUENCE NUMBER", true),
             new IFB_NUMERIC (  3, "NETWORK INTERNATIONAL IDENTIFIEER", true),
             new IFB_NUMERIC (  2, "POINT OF SERVICE CONDITION CODE", true),
             new IFB_NUMERIC (  2, "POINT OF SERVICE PIN CAPTURE CODE", true),
             new IFB_NUMERIC (  1, "AUTHORIZATION IDENTIFICATION RESP LEN",true),
-            new IFE_CHAR    (  9, "AMOUNT, TRANSACTION FEE"),
+   /*de28*/ new IFE_CHAR    (  9, "AMOUNT, TRANSACTION FEE"),
             new IFE_CHAR  (  9, "AMOUNT, SETTLEMENT FEE"),
-            new IFE_CHAR  (  9, "AMOUNT, TRANSACTION PROCESSING FEE"),
-            new IFE_CHAR  (  9, "AMOUNT, SETTLEMENT PROCESSING FEE"),
-            new IFB_NUMERIC   ( 11, "ACQUIRING INSTITUTION IDENT CODE", true),
-            new IFB_LLNUM   ( 11, "FORWARDING INSTITUTION IDENT CODE", pad),
+            new IFE_CHAR  (  10, "AMOUNT, TRANSACTION PROCESSING FEE"),
+            new IFE_CHAR  ( 11, "AMOUNT, SETTLEMENT PROCESSING FEE"),
+  /*de032*/ new IFB_LLNUM   ( 11, "ACQUIRING INSTITUTION IDENT CODE",true),
+            new IFE_CHAR   ( 11, "FORWARDING INSTITUTION IDENT CODE"),
             new IFB_LLCHAR  ( 28, "PAN EXTENDED"),
-            new IFA_FLLCHAR   ( 37, "TRACK 2 DATA"),
-            new IFB_LLLCHAR (104, "TRACK 3 DATA"),
+   /*de035*/new IFB_LLHEX   ( 37, "TRACK 2 DATA"),
+            new IFB_NUMERIC (104, "TRACK 3 DATA",true),
             new IFE_CHAR     ( 12, "RETRIEVAL REFERENCE NUMBER"),
             new IFE_CHAR     (  6, "AUTHORIZATION IDENTIFICATION RESPONSE"),
             new IFE_CHAR     (  2, "RESPONSE CODE"),
@@ -71,93 +88,33 @@ public class ISOIss  extends ISOBasePackager{
             new IFE_CHAR     (  8, "CARD ACCEPTOR TERMINAL IDENTIFICACION"),
             new IFE_CHAR     ( 15, "CARD ACCEPTOR IDENTIFICATION CODE" ),
             new IFE_CHAR     ( 40, "CARD ACCEPTOR NAME/LOCATION"),
-            new IFB_LLCHAR  ( 25, "ADITIONAL RESPONSE DATA"),
-            new IFB_LLCHAR  ( 76, "TRACK 1 DATA"),
-            new IFB_LLLCHAR (999, "ADITIONAL DATA - ISO"),
-            new IFB_LLLCHAR (999, "ADITIONAL DATA - NATIONAL"),
-            new IFB_LLLCHAR (999, "ADITIONAL DATA - PRIVATE"),
+/*44ANS V*/ new IFE_CHAR  ( 25, "ADITIONAL RESPONSE DATA"),
+            new IFE_CHAR  ( 76, "TRACK 1 DATA"),
+            new IFE_CHAR (1, "ADITIONAL DATA - ISO"),
+            new IFE_CHAR (1, "ADITIONAL DATA - NATIONAL"),
+            new IFE_CHAR (1, "ADITIONAL DATA - PRIVATE"),
             new IFB_NUMERIC     (  3, "CURRENCY CODE, TRANSACTION",true),
             new IFB_NUMERIC     (  3, "CURRENCY CODE, SETTLEMENT",true),
             new IFB_NUMERIC     (  3, "CURRENCY CODE, CARDHOLDER BILLING" ,true  ),
             new IFB_BINARY  (  8, "PIN DATA"   ),
-            new IFB_NUMERIC ( 16, "SECURITY RELATED CONTROL INFORMATION", true),
-          
-            new IFB_LLLCHAR (120, "ADDITIONAL AMOUNTS"),
-            new IFB_LLLCHAR (999, "RESERVED ISO"),
-            new IFB_LLLCHAR (999, "RESERVED ISO"),
-            new IFB_LLLCHAR (999, "RESERVED NATIONAL"),
-  /*60*/    new IFB_NUMERIC (999, "RESERVED NATIONAL",true),
-            new IFB_LLLCHAR (999, "RESERVED NATIONAL"),
-            new IFB_LLLCHAR (999, "RESERVED PRIVATE"),
-            new IFB_LLLCHAR (999, "RESERVED PRIVATE"),
-          
-     /*62*/ new IFB_NUMERIC (999, "RESERVED PRIVATE",pad),
-            new IFB_NUMERIC (999, "RESERVED PRIVATE",pad),
-            new IFB_BINARY  (  8, "MESSAGE AUTHENTICATION CODE FIELD"),
-            new IFB_BINARY  (  1, "BITMAP, EXTENDED"),
-            new IFB_NUMERIC (  1, "SETTLEMENT CODE", true),
-            new IFB_NUMERIC (  2, "EXTENDED PAYMENT CODE", true),
-            new IFB_NUMERIC (  3, "RECEIVING INSTITUTION COUNTRY CODE", true),
-            new IFB_NUMERIC (  3, "SETTLEMENT INSTITUTION COUNTRY CODE", true),
-            new IFB_NUMERIC (  3, "NETWORK MANAGEMENT INFORMATION CODE", true),
-            new IFB_NUMERIC (  4, "MESSAGE NUMBER", true),
-            new IFB_NUMERIC (  4, "MESSAGE NUMBER LAST", true),
-            new IFB_NUMERIC (  6, "DATE ACTION", true),
-            new IFB_NUMERIC ( 10, "CREDITS NUMBER", true),
-            new IFB_NUMERIC ( 10, "CREDITS REVERSAL NUMBER", true),
-            new IFB_NUMERIC ( 10, "DEBITS NUMBER", true),
-            new IFB_NUMERIC ( 10, "DEBITS REVERSAL NUMBER", true),
-            new IFB_NUMERIC ( 10, "TRANSFER NUMBER", true),
-            new IFB_NUMERIC ( 10, "TRANSFER REVERSAL NUMBER", true),
-            new IFB_NUMERIC ( 10, "INQUIRIES NUMBER", true),
-            new IFB_NUMERIC ( 10, "AUTHORIZATION NUMBER", true),
-            new IFB_NUMERIC ( 12, "CREDITS, PROCESSING FEE AMOUNT", true),
-            new IFB_NUMERIC ( 12, "CREDITS, TRANSACTION FEE AMOUNT", true),
-            new IFB_NUMERIC ( 12, "DEBITS, PROCESSING FEE AMOUNT", true),
-            new IFB_NUMERIC ( 12, "DEBITS, TRANSACTION FEE AMOUNT", true),
-            new IFB_NUMERIC ( 16, "CREDITS, AMOUNT", true),
-            new IFB_NUMERIC ( 16, "CREDITS, REVERSAL AMOUNT", true),
-            new IFB_NUMERIC ( 16, "DEBITS, AMOUNT", true),
-            new IFB_NUMERIC ( 16, "DEBITS, REVERSAL AMOUNT", true),
-            new IFB_NUMERIC ( 42, "ORIGINAL DATA ELEMENTS", true),
-            new IFE_CHAR     (  1, "FILE UPDATE CODE"),
-            new IFE_CHAR     (  2, "FILE SECURITY CODE"),
-            new IFE_CHAR     (  6, "RESPONSE INDICATOR"),
-            new IFE_CHAR     (  7, "SERVICE INDICATOR"),
-            new IFE_CHAR     ( 42, "REPLACEMENT AMOUNTS"),
-            new IFB_BINARY  ( 16, "MESSAGE SECURITY CODE"),
-            new IFB_AMOUNT  ( 17, "AMOUNT, NET SETTLEMENT", pad),
-            new IFE_CHAR     ( 25, "PAYEE"),
-            new IFB_LLNUM   ( 11, "SETTLEMENT INSTITUTION IDENT CODE", pad),
-            new IFB_LLNUM   ( 11, "RECEIVING INSTITUTION IDENT CODE", pad),
-            new IFB_LLCHAR  ( 17, "FILE NAME"),
-            new IFB_LLCHAR  ( 28, "ACCOUNT IDENTIFICATION 1"),
-            new IFB_LLCHAR  ( 28, "ACCOUNT IDENTIFICATION 2"),
-            new IFB_LLLCHAR (100, "TRANSACTION DESCRIPTION"),
-            new IFB_LLLCHAR (999, "RESERVED ISO USE"), 
-            new IFB_LLLCHAR (999, "RESERVED ISO USE"), 
-            new IFB_LLLCHAR (999, "RESERVED ISO USE"), 
-            new IFB_LLLCHAR (999, "RESERVED ISO USE"), 
-            new IFB_LLLCHAR (999, "RESERVED ISO USE"), 
-            new IFB_LLLCHAR (999, "RESERVED ISO USE"), 
-            new IFB_LLLCHAR (999, "RESERVED ISO USE"), 
-            new IFB_LLLCHAR (999, "RESERVED NATIONAL USE"),
-            new IFB_LLLCHAR (999, "RESERVED NATIONAL USE"),
-            new IFB_LLLCHAR (999, "RESERVED NATIONAL USE"   ),
-            new IFB_LLLCHAR (999, "RESERVED NATIONAL USE"),
-            new IFB_LLLCHAR (999, "RESERVED NATIONAL USE"  ),
-            new IFB_LLLCHAR (999, "RESERVED NATIONAL USE"),
-            new IFB_LLLCHAR (999, "RESERVED NATIONAL USE"),
-            new IFB_LLLCHAR (999, "RESERVED NATIONAL USE"),
-            new IFB_LLLCHAR (999, "RESERVED PRIVATE USE"),
-            new IFB_LLLCHAR (999, "RESERVED PRIVATE USE"),
-            new IFB_LLLCHAR (999, "RESERVED PRIVATE USE"),
-            new IFB_LLLCHAR (999, "RESERVED PRIVATE USE"),
-            new IFB_LLLCHAR (999, "RESERVED PRIVATE USE"),
-            new IFB_LLLCHAR (999, "RESERVED PRIVATE USE"),
-            new IFB_LLLCHAR (999, "RESERVED PRIVATE USE"),
-            new IFB_LLLCHAR (999, "RESERVED PRIVATE USE"),
-            new IFB_BINARY  (  8, "MAC 2")
+/*53*/      new IFB_NUMERIC ( 16, "SECURITY RELATED CONTROL INFORMATION",true),    
+            new IFE_CHAR (120, "ADDITIONAL AMOUNTS"),
+/*add55*/   new IFB_LLHBINARY (255, "INTEGRATED CIRCUIT CARD (ICC)-RELATED DATA"),
+            new IFE_CHAR (255, "RESERVED ISO"),
+            new IFE_CHAR (255, "RESERVED ISO"),
+            new IFE_CHAR (255, "RESERVED NATIONAL"),
+  /*59*/    new IFE_CHAR (255, "RESERVED NATIONAL"),
+  /*add 60*/new IFB_HEX (12, "NATIONAL POINT-OF-SERVICE GEOGRAPHIC DATA",pad),
+   /*add61*/new IFB_LLNUM (36, "ADDITIONAL POS INFORMATION",true),
+            new IFB_LLLNUM (255, "RESERVED NATIONAL",true),
+     /*63*/ new IFB_LLLNUM (256, "RESERVED PRIVATE",true),
+    
+            
+
+    
+
+            
+
         };
 
     public ISOIss() {
--- src/main/java/com/mycompany/iso8583/parse.java
+++ src/main/java/com/mycompany/iso8583/parse.java
@@ -5,6 +5,7 @@
  */
 package com.mycompany.iso8583;
 
+import java.util.BitSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jpos.iso.ISOException;
@@ -20,28 +21,67 @@ import org.jpos.iso.packager.ISO87BPackager;
 public class parse {
     
    public static void main(String[] args) {
-        packmess();
-        
+       
+       try {
+           
+           // Create ISO Message
+           ISOMsg msg = new ISOMsg();
+           GenericPackager packager = new GenericPackager("/home/azelinsky/ISS/parseISO/src/resources/grab.xml");
+           
+          // msg.setPackager(packager);
+           msg.setPackager(new ISOIss());
+          // msg.unpack(packmessSmoll());
+            msg.unpack(packmess());
+          
+           String cat = msg.getMTI();
+           for (int i=1;i<=msg.getMaxField();i++) {
+			if (msg.hasField(i)) {
+				cat = cat +"|"+msg.getString(i);
+				   System.out.println("    Field-"+i+" : "+msg.getString(i));
+			}
+			
+		}
+           System.out.print(cat);
+       } catch (ISOException ex) {
+           Logger.getLogger(parse.class.getName()).log(Level.SEVERE, null, ex);
+       }
+      
+               
+              
+	
     
     }
-public static void packmess() {
-          String mess ="014B0000160102014B0000008598220000000000000000000000"
-                  + "0100"
-                  + "763C669128E0BA16"
-                  + "104785299000235458"
-                  + "010000000000000011000000000011120416382300077216382312042005601108400700000100C4F0F0F0F0F0F0F0F00B0123456789012504785299000235458D20052011372500000201F9F3F3F8F1F6F0F0F0F7F7F2C1E3D4F0F1404040C3C1D9C440C1C3C3C5D7E3D6D94040C1C3D8E4C9D9C5D940D5C1D4C5404040404040404040404040C3C9E3E840D5C1D4C540404040E4E208400840DAE7386BEA0B25162001010100000000660100639F7C0A000000000000000000009F3303204000950580000100009F37049BADBCAB9F100706011103A0B0009F260892B379D36CA348049F3602000D82023C009C01009F1A0208269A031804279F02060000000000005F2A0208268407A00000000310100128080000000000000000058000000002";
+public static byte[] packmess() {
+    
+
+       String message1="0100763C669128E0BA16104785299000235458010000000000000011000000000011120416382300077216382312042005601108400700000100C4F0F0F0F0F0F0F0F00B0123456789012504785299000235458D20052011372500000201F9F3F3F8F1F6F0F0F0F7F7F2C1E3D4F0F1404040C3C1D9C440C1C3C3C5D7E3D6D94040C1C3D8E4C9D9C5D940D5C1D4C5404040404040404040404040C3C9E3E840D5C1D4C540404040E4E208400840DAE7386BEA0B25162001010100000000660100639F7C0A000000000000000000009F3303204000950580000100009F37049BADBCAB9F100706011103A0B0009F260892B379D36CA348049F3602000D82023C009C01009F1A0208269A031804279F02060000000000005F2A0208268407A00000000310100128080000000000000000058000000002";
+       byte[] data = null;
        
+       //String message=ISOUtil.hexString(packmessSmoll());
+       data=message1.getBytes();
+        
+  return  data;
+      
+      }
+public static byte[] packmessSmoll() {
+          
+           String message="";
+           byte[] data = null;
     try {
+        
+        GenericPackager packager = new GenericPackager("/home/azelinsky/ISS/parseISO/src/resources/grab.xml");
+        
         ISOMsg isoMsg = new ISOMsg();
+        //isoMsg.setPackager(packager);
         isoMsg.setPackager(new ISOIss());
         isoMsg.setMTI("0100");
         isoMsg.set(2, "4785299000235458");
         isoMsg.set(3, "010000");
         isoMsg.set(4, "000000000011");
         isoMsg.set(6, "000000000011");
-        isoMsg.set(7, "1204163823");
+        isoMsg.set(7, "1204163823");        
         isoMsg.set(11, "000772");
-        isoMsg.set(12, "163823");
+        isoMsg.set(12, "163823"); 
         isoMsg.set(13, "1204");
         isoMsg.set(14, "2005");
         isoMsg.set(18, "6011");
@@ -52,10 +92,10 @@ public static void packmess() {
         isoMsg.set(28, "D00000000");
         isoMsg.set(32, "12345678901");
         isoMsg.set(35, "4785299000235458D20052011372500000201");
-        isoMsg.set(37, "933816000772");
+        isoMsg.set(37, "933816000772");   
         isoMsg.set(41, "ATM01");
         isoMsg.set(42, "CARD ACCEPTOR  ");
-        isoMsg.set(43, "ACQUIRER NAME            CITY NAME    US");
+        isoMsg.set(43, "ACQUIRER NAME            CITY NAME    US");  
         isoMsg.set(49, "840");
         isoMsg.set(51, "840");
         isoMsg.set(52, "DAE7386BEA0B2516");
@@ -64,51 +104,15 @@ public static void packmess() {
         isoMsg.set(60, "28");
         isoMsg.set(62, "0000000000000000");
         isoMsg.set(63, "8000000002");
-        
-        
+
         // Get and print the output result
-        byte[] data = isoMsg.pack();
-        String message=ISOUtil.hexString(data);
-        System.out.println("RESULT : " + message);
+        data = isoMsg.pack();
+         message=ISOUtil.hexString(data);
+        System.out.println("RESULT1 : " + message);
     } catch (ISOException ex) {
         Logger.getLogger(parse.class.getName()).log(Level.SEVERE, null, ex);
     }
-        
-  
-      
-      }
-public static void packmessSmoll() {
-          String mess ="014B0000160102014B0000008598220000000000000000000000"
-                  + "0100"
-                  + "763C669128E0BA16"
-                  + "104785299000235458"
-                  + "010000000000000011000000000011120416382300077216382312042005601108400700000100C4F0F0F0F0F0F0F0F00B0123456789012504785299000235458D20052011372500000201F9F3F3F8F1F6F0F0F0F7F7F2C1E3D4F0F1404040C3C1D9C440C1C3C3C5D7E3D6D94040C1C3D8E4C9D9C5D940D5C1D4C5404040404040404040404040C3C9E3E840D5C1D4C540404040E4E208400840DAE7386BEA0B25162001010100000000660100639F7C0A000000000000000000009F3303204000950580000100009F37049BADBCAB9F100706011103A0B0009F260892B379D36CA348049F3602000D82023C009C01009F1A0208269A031804279F02060000000000005F2A0208268407A00000000310100128080000000000000000058000000002";
-     
-
-    try {
-        
-        GenericPackager packager = new GenericPackager("C:\\Users\\adolf\\Documents\\NetBeansProjects\\iso8583\\src\\resources\\XmlSchema.xsd");
-        
-        ISOMsg isoMsg = new ISOMsg();
-        isoMsg.setPackager(packager);
-        isoMsg.setMTI("0100");
-        isoMsg.set(2, "4785299000235458");
-        isoMsg.set(3, "010000");
-        isoMsg.set(4, "000000000011");
-        isoMsg.set(6, "000000000011");
-        isoMsg.set(7, "1204163823");
-        isoMsg.set(28, "D00000000");
-
-        
-        
-        // Get and print the output result
-        byte[] data = isoMsg.pack();
-        String message=ISOUtil.hexString(data);
-        System.out.println("RESULT : " + message);
-    } catch (ISOException ex) {
-        Logger.getLogger(parse.class.getName()).log(Level.SEVERE, null, ex);
-    }
-        
+        return  data;
   
       
       }
