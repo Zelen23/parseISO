3cf14a738ec377634cd44bd61990681607bbdd6d
Alexander Zelinskiy <a.zelinskiy@it.ecommpay.com>
2019-12-06 19:43:42 +0300

    draft

--- src/main/java/com/mycompany/iso8583/ISOIss.java
+++ src/main/java/com/mycompany/iso8583/ISOIss.java
@@ -5,8 +5,12 @@
  */
 package com.mycompany.iso8583;
 
+import org.jpos.iso.IFAE_LLCHAR;
+import org.jpos.iso.IFA_BINARY;
 import org.jpos.iso.IFA_BITMAP;
 import org.jpos.iso.IFA_FLLCHAR;
+import org.jpos.iso.IFA_FLLNUM;
+import org.jpos.iso.IFA_LLABINARY;
 import org.jpos.iso.IFA_LLBINARY;
 import org.jpos.iso.IFA_LLCHAR;
 import org.jpos.iso.IFA_LLLBINARY;
@@ -17,28 +21,42 @@ import org.jpos.iso.IFA_NUMERIC;
 import org.jpos.iso.IFB_AMOUNT;
 import org.jpos.iso.IFB_BINARY;
 import org.jpos.iso.IFB_BITMAP;
+import org.jpos.iso.IFB_FLLNUM;
 import org.jpos.iso.IFB_HEX;
 import org.jpos.iso.IFB_LLBINARY;
 import org.jpos.iso.IFB_LLCHAR;
 import org.jpos.iso.IFB_LLHBINARY;
+import org.jpos.iso.IFB_LLHCHAR;
+import org.jpos.iso.IFB_LLHECHAR;
 import org.jpos.iso.IFB_LLHEX;
+import org.jpos.iso.IFB_LLHFBINARY;
+import org.jpos.iso.IFB_LLHNUM;
 import org.jpos.iso.IFB_LLLBINARY;
 import org.jpos.iso.IFB_LLLCHAR;
+import org.jpos.iso.IFB_LLLHBINARY;
+import org.jpos.iso.IFB_LLLHECHAR;
+import org.jpos.iso.IFB_LLLHNUM;
 import org.jpos.iso.IFB_LLLLCHAR;
 import org.jpos.iso.IFB_LLLNUM;
 import org.jpos.iso.IFB_LLNUM;
 import org.jpos.iso.IFB_NUMERIC;
+import org.jpos.iso.IFEA_LLCHAR;
+import org.jpos.iso.IFEP_LLLBINARY;
+import org.jpos.iso.IFE_BINARY;
 import org.jpos.iso.IFE_BITMAP;
 import org.jpos.iso.IFE_CHAR;
 import org.jpos.iso.IFE_CHAR;
+import org.jpos.iso.IFE_LLBINARY;
 import org.jpos.iso.IFE_LLLBINARY;
 import org.jpos.iso.IFE_LLLCHAR;
+import org.jpos.iso.IFE_LLLEBINARY;
 import org.jpos.iso.IFE_LLLLCHAR;
 import org.jpos.iso.IFE_LLLNUM;
 import org.jpos.iso.IFE_LLNUM;
 import org.jpos.iso.IFE_NUMERIC;
 import org.jpos.iso.IF_CHAR;
 import org.jpos.iso.IF_ECHAR;
+import org.jpos.iso.IF_TBASE;
 import org.jpos.iso.ISOBasePackager;
 import org.jpos.iso.ISOFieldPackager;
 
@@ -51,7 +69,7 @@ public class ISOIss  extends ISOBasePackager{
     protected ISOFieldPackager fld[] = {
             new IFB_NUMERIC (  4, "MESSAGE TYPE INDICATOR",true),
             new IFB_BITMAP  ( 16, "BIT MAP"),
-            new IFB_LLNUM   ( 19, "PAN - PRIMARY ACCOUNT NUMBER",true),
+            new IFB_LLHNUM   ( 19, "PAN - PRIMARY ACCOUNT NUMBER",pad),
             new IFB_NUMERIC (  6, "PROCESSING CODE", true),
             new IFB_NUMERIC ( 12, "AMOUNT, TRANSACTION", true),
             new IFB_NUMERIC ( 12, "AMOUNT, SETTLEMENT", true),
@@ -81,10 +99,10 @@ public class ISOIss  extends ISOBasePackager{
             new IFE_CHAR  (  9, "AMOUNT, SETTLEMENT FEE"),
             new IFE_CHAR  (  10, "AMOUNT, TRANSACTION PROCESSING FEE"),
             new IFE_CHAR  ( 11, "AMOUNT, SETTLEMENT PROCESSING FEE"),
-  /*de032*/ new IFB_LLNUM   ( 11, "ACQUIRING INSTITUTION IDENT CODE",true),
+  /*de032*/ new IFB_LLHNUM   ( 11, "ACQUIRING INSTITUTION IDENT CODE",true),
             new IFE_CHAR   ( 11, "FORWARDING INSTITUTION IDENT CODE"),
             new IFB_LLCHAR  ( 28, "PAN EXTENDED"),
-   /*de035*/new IFB_LLHEX   ( 37, "TRACK 2 DATA"),
+   /*de035*/new IFB_LLHNUM (37, "TRACK 2 DATA",true),
             new IFB_NUMERIC (104, "TRACK 3 DATA",true),
             new IFE_CHAR     ( 12, "RETRIEVAL REFERENCE NUMBER"),
             new IFE_CHAR     (  6, "AUTHORIZATION IDENTIFICATION RESPONSE"),
@@ -97,10 +115,10 @@ public class ISOIss  extends ISOBasePackager{
             new IFE_CHAR  ( 76, "TRACK 1 DATA"),
             new IFE_CHAR (1, "ADITIONAL DATA - ISO"),
             new IFE_CHAR (1, "ADITIONAL DATA - NATIONAL"),
-            new IFE_CHAR (1, "ADITIONAL DATA - PRIVATE"),
-            new IFB_NUMERIC     (  3, "CURRENCY CODE, TRANSACTION",true),
-            new IFB_NUMERIC     (  3, "CURRENCY CODE, SETTLEMENT",true),
-            new IFB_NUMERIC     (  3, "CURRENCY CODE, CARDHOLDER BILLING" ,true  ),
+            new IFB_NUMERIC (1, "ADITIONAL DATA - PRIVATE",true),
+            new IFB_HEX     (  3, "CURRENCY CODE, TRANSACTION",true),
+            new IFB_HEX     (  3, "CURRENCY CODE, SETTLEMENT",true),
+            new IFB_HEX     (  3, "CURRENCY CODE, CARDHOLDER BILLING" ,true  ),
             new IFB_BINARY  (  8, "PIN DATA"   ),
 /*53*/      new IFB_NUMERIC ( 16, "SECURITY RELATED CONTROL INFORMATION",true),    
             new IFE_CHAR (120, "ADDITIONAL AMOUNTS"),
